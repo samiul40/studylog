95806b613fe93ad7a269c5473f10d72ac28c4ad4
Samiul Amin <samiulamin@hotmail.co.uk>
2026-06-01 12:29:47 +0100

    feat: add landing page with animated hero

--- web/pages/tests/test_views.py
+++ web/pages/tests/test_views.py
@@ -6,11 +6,11 @@ pytestmark = pytest.mark.django_db
 INDEX_URL = reverse("index")
 
 
-def test_index_redirects_to_dashboard(client):
+def test_index_renders_landing_page_for_anonymous(client):
     response = client.get(INDEX_URL)
 
-    assert response.status_code == 302
-    assert response.url == reverse("learning:dashboard")
+    assert response.status_code == 200
+    assert b"StudyLog" in response.content
 
 
 def test_index_redirects_authenticated_user_to_dashboard(client_logged_in):
