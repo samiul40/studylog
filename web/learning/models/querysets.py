a445759a702284976012f781aa7323baca7f20b8
Samiul Amin <samiulamin@hotmail.co.uk>
2026-06-07 21:48:17 +0100

    redesign: resource list mobile fixes, remove auth page animations, and various UI/UX improvements

--- web/learning/models/querysets.py
+++ web/learning/models/querysets.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.db.models import (
     Case,
@@ -6,9 +8,12 @@ from django.db.models import (
     F,
     IntegerField,
     Q,
+    Sum,
     Value,
     When,
 )
+from django.db.models.functions import Coalesce
+from django.utils import timezone
 
 
 class LearningResourceQuerySet(models.QuerySet):
@@ -35,5 +40,41 @@ class LearningResourceQuerySet(models.QuerySet):
                     100.0 * F("completed_units") / F("total_units"),
                     output_field=IntegerField(),
                 ),
+            )
+        )
+
+    def with_weekly_units(self):
+        """Annotate each resource with units completed in the last 7 days."""
+        seven_days_ago = timezone.now() - datetime.timedelta(days=7)
+        return self.annotate(
+            units_this_week=Count(
+                "units",
+                filter=Q(
+                    units__status="completed",
+                    units__updated_at__gte=seven_days_ago,
+                ),
+            )
+        )
+
+    def with_time_logged(self):
+        """
+        Annotate each resource with total minutes of time actually logged:
+        - completed units contribute their full duration_minutes
+        - in-progress units contribute their video_progress_minutes
+        """
+        return self.annotate(
+            time_logged_minutes=Sum(
+                Case(
+                    When(
+                        units__status="completed",
+                        then=Coalesce(F("units__duration_minutes"), Value(0)),
+                    ),
+                    When(
+                        units__status="in_progress",
+                        then=Coalesce(F("units__video_progress_minutes"), Value(0)),
+                    ),
+                    default=Value(0),
+                    output_field=IntegerField(),
+                )
             )
         )
